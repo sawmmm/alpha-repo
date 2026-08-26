5c78ae196608f50e329bfa940df95d5f695674db
samwel <sawm.sza@gmail.com>
2026-08-25 22:44:57 -0300

    Verso de Daeonica - Tarsus

--- src/Daeonica.java
+++ src/Daeonica.java
@@ -2,7 +2,7 @@ public class Daeonica {
 
     public static void main(String[] args) {
 
-        System.out.println("Sobre ele visitarei a fome e o fogo. Até que ao seu redor ressoe a desolação, e todos os demônios na treva exterior olhem espantados e reconheçam que a vingança é a função do homem.");
+        System.out.println("Sobre ele visitarei a fome e o fogo. Até que ao seu redor ressoe a desolação, e todos os demônios na treva exterior olhem espantados e reconheçam que a vingança é a função do homem. - Tarsus");
 
     }
 
